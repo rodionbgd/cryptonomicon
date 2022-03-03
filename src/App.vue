955a346dc49d46a5afb228f3e83bf2734c4f2dd6
rodionbgd <rodionbgd@mail.ru>
2022-03-03 19:47:05 +0300

    fix(style): Подключение tailwind

--- src/App.vue
+++ src/App.vue
@@ -191,5 +191,3 @@ export default {
   },
 };
 </script>
-
-<style src="./app.css"></style>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+      require('@tailwindcss/forms'),
+  ],
+}
